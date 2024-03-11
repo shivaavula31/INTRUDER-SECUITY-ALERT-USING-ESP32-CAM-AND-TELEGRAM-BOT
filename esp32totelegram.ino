b565496371a5367f11c779464d5f2a1297210261
shivakumar1214 <107173221+shivaavula31@users.noreply.github.com>
2024-03-11 20:41:11 +0530

    Update esp32totelegram.ino

--- esp32totelegram.ino
+++ esp32totelegram.ino
@@ -1,12 +1,7 @@
 /*
 ESP32-CAM save a captured photo to Telegram
-Author : ChungYi Fu (Kaohsiung, Taiwan)  2021-7-5 15:00
-https://www.facebook.com/francefu
-Telegram Bot API
-https://core.telegram.org/bots/api
 */
 
-//輸入WIFI連線帳號密碼
 const char* ssid     = "akshay";   //your network SSID
 const char* password = "12345678";   //your network password
 
@@ -15,11 +10,10 @@ String myChatId = "5862373682";   // Get chat_id -> https://telegram.me/userinfo
 
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
-#include "soc/soc.h"             //用於電源不穩不重開機 
-#include "soc/rtc_cntl_reg.h"    //用於電源不穩不重開機 
-#include "esp_camera.h"          //視訊函式
+#include "soc/soc.h"            
+#include "soc/rtc_cntl_reg.h"     
+#include "esp_camera.h"         
 
-//ESP32-CAM 安信可模組腳位設定
 #define PWDN_GPIO_NUM     32
 #define RESET_GPIO_NUM    -1
 #define XCLK_GPIO_NUM      0
@@ -40,13 +34,13 @@ String myChatId = "5862373682";   // Get chat_id -> https://telegram.me/userinfo
 
 void setup() 
 {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);  //關閉電源不穩就重開機的設定
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);  
     
   Serial.begin(115200);
-  Serial.setDebugOutput(true);  //開啟診斷輸出
+  Serial.setDebugOutput(true); 
   Serial.println();
 
-  //視訊組態設定  https://github.com/espressif/esp32-camera/blob/master/driver/include/esp_camera.h
+  // https://github.com/espressif/esp32-camera/blob/master/driver/include/esp_camera.h
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -76,7 +70,7 @@ void setup()
   //   
   // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
   //                      for larger pre-allocated frame buffer.
-  if(psramFound()){  //是否有PSRAM(Psuedo SRAM)記憶體IC
+  if(psramFound()){  
     config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
@@ -86,14 +80,12 @@ void setup()
     config.fb_count = 1;
   }
 
-  //視訊初始化
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     ESP.restart();
   }
 
-  //可自訂視訊框架預設大小(解析度大小)
   sensor_t * s = esp_camera_sensor_get();
   // initial sensors are flipped vertically and colors are a bit saturated
   if (s->id.PID == OV3660_PID) {
@@ -104,20 +96,19 @@ void setup()
   // drop down frame size for higher initial frame rate
   s->set_framesize(s, FRAMESIZE_SVGA);    //解析度 UXGA(1600x1200), SXGA(1280x1024), XGA(1024x768), SVGA(800x600), VGA(640x480), CIF(400x296), QVGA(320x240), HQVGA(240x176), QQVGA(160x120), QXGA(2048x1564 for OV3660)
 
-  //s->set_vflip(s, 1);  //垂直翻轉
-  //s->set_hmirror(s, 1);  //水平鏡像
+  //s->set_vflip(s, 1);  
+  //s->set_hmirror(s, 1);  
   
-  //閃光燈(GPIO4)
+  //(GPIO4)
   ledcAttachPin(4, 4);  
   ledcSetup(4, 5000, 8);
   
-  WiFi.mode(WIFI_AP_STA);  //其他模式 WiFi.mode(WIFI_AP); WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_AP_STA);  // WiFi.mode(WIFI_AP); WiFi.mode(WIFI_STA);
 
-  //指定Client端靜態IP
   //WiFi.config(IPAddress(192, 168, 201, 100), IPAddress(192, 168, 201, 2), IPAddress(255, 255, 255, 0));
 
   for (int i=0;i<2;i++) {
-    WiFi.begin(ssid, password);    //執行網路連線
+    WiFi.begin(ssid, password);    
   
     delay(1000);
     Serial.println("");
@@ -130,13 +121,13 @@ void setup()
         if ((StartTime+5000) < millis()) break;    //等待10秒連線
     } 
   
-    if (WiFi.status() == WL_CONNECTED) {    //若連線成功
+    if (WiFi.status() == WL_CONNECTED) {    
       Serial.println("");
       Serial.println("STAIP address: ");
       Serial.println(WiFi.localIP());
       Serial.println("");
   
-      for (int i=0;i<5;i++) {   //若連上WIFI設定閃光燈快速閃爍
+      for (int i=0;i<5;i++) {  
         ledcWrite(4,10);
         delay(200);
         ledcWrite(4,0);
@@ -146,8 +137,8 @@ void setup()
     }
   } 
 
-  if (WiFi.status() != WL_CONNECTED) {    //若連線失敗
-    for (int i=0;i<2;i++) {    //若連不上WIFI設定閃光燈慢速閃爍
+  if (WiFi.status() != WL_CONNECTED) {    
+    for (int i=0;i<2;i++) {   
       ledcWrite(4,10);
       delay(1000);
       ledcWrite(4,0);
@@ -156,7 +147,6 @@ void setup()
     ESP.restart();
   } 
 
-  //設定閃光燈為低電位
   pinMode(4, OUTPUT);
   digitalWrite(4, LOW); 
 
